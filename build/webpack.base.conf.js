f1cf32a52358d3f9ee34de9b42d6c6f1aedd48c2
Jacob <jacob@i-click.com>
2018-10-22 16:39:52 +0800

    add

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,9 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'components': resolve('src/components'),
+      'assets': resolve('src/assets'),
+      'common': resolve('src/common'),
     }
   },
   module: {
---
+++ src/views/recommend.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class='recommend' ref='recommend'>
+	</div>
+</template>
+
+<script type="text/javascript">
+  export default {}
+</script>
+
+<style lang="scss" scoped>
+</style>
